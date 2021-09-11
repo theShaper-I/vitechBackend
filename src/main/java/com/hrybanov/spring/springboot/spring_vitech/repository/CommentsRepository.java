a429dda183b26e89092fa623fa9a5867ee2aae2c
Artem <example@gmail.com>
2021-09-11 13:58:49 +0300

    Add CorsConfig

--- src/main/java/com/hrybanov/spring/springboot/spring_vitech/repository/CommentsRepository.java
+++ src/main/java/com/hrybanov/spring/springboot/spring_vitech/repository/CommentsRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 public interface CommentsRepository extends JpaRepository<Comment, Integer> {
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @Modifying
     @Transactional
     @Query(value = "insert into my_db.comments (text, data, patient_id) VALUES (:text, :data, :id)", nativeQuery = true)
